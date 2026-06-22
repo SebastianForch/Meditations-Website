92ef212d26bb34f8cd4b2bd3348a86c66e67afe4
Sebastian.Forch <s.forch@schigy-og.de>
2026-06-22 17:50:52 +0200

    Pop-Up Sitzpositionen

--- script.js
+++ script.js
@@ -18,3 +18,21 @@ if ('serviceWorker' in navigator) {
       });
   });
 }
+
+// Funktion zum Öffnen des Pop-Ups
+        function openPopup() {
+            document.getElementById("popup").style.display = "block";
+        }
+
+        // Funktion zum Schließen des Pop-Ups
+        function closePopup() {
+            document.getElementById("popup").style.display = "none";
+        }
+
+        // Schließen, wenn außerhalb des Pop-Ups geklickt wird
+        window.onclick = function(event) {
+            const popup = document.getElementById("popup");
+            if (event.target === popup) {
+                closePopup();
+            }
+        }
